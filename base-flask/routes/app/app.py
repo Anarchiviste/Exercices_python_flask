8d5faedf26d1483b8c4a11263252a40272ec2852
Jules Musquin <julesmusquin@protonmail.com>
2025-12-09 17:53:44 +0100

    ajout de dossier routes

---
+++ base-flask/routes/app/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import request
+from flask import url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+
+app = Flask(
+	__name__,
+	template_folder='templates',
+    static_folder='statics'
+)
+
+'''
+tentative d'éviter le bug chelou
+'''
+
+'''class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(app)'''
+
+from .route import generale
